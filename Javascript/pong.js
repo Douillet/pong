86e9d1b9f394e27ccbb6a724f93da09828d11874
Douillet <douillet.a09092000@gmail.com>
2020-11-12 18:25:22 +0100

    Add files via upload

--- Javascript/pong.js
+++ Javascript/pong.js
@@ -1,12 +1,8 @@
-let largeur=$("#balle").width();
-let gauche=parseInt($("#balle").css("left"));
-let haut=parseInt($("#balle").css("top"));
-
-
 
 
 class Terrain{
     constructor($element){
+        //Appelle les varaibles du fichier CSS
         this.$element=$element;
         this.largeur=$element.width();
         this.hauteur=$element.height();
@@ -15,39 +11,53 @@ class Terrain{
 }
 class Balle{
     constructor($html){
+        /*Permet d'appeler les informations saisies dans le code CSS pour faire correspondre
+        les données de colisions et avec les données graphiques*/
         this.$html=$html;
         this.haut=parseInt($("#balle").css("left"));
         this.gauche=parseInt($("#balle").css("top"));
-        this.vitesseX=2;
-        this.vitesseY=0.5;
+        this.largeur=parseInt($("#balle").css("width"));
+        this.hauteur=parseInt($("#balle").css("height"));
+        //Configure la vitesse de la balle
+        this.vitesseX=5;  //selon la largeur
+        this.vitesseY=2;  //selon la hauteur
     }
+    //Permet d'utiliser ces termes dans une fonction quanf majHTML(){} est entré
     majHTML(){
         this.$html.css("left",balle.gauche);
         this.$html.css("top",balle.haut);
+        this.$html.css("width",balle.largeur);
+        this.$html.css("height",balle.hauteur);
     }
 }
 
-let terrain=new Terrain($("#terrain"));
-let balle=new Balle($("#balle"));
+let terrain=new Terrain($("#terrain"));  //Créé le terrain
+let balle=new Balle($("#balle"));        //Créé la balle
 
 setInterval(function(){
-    balle.gauche=balle.gauche+vitesseX; //Donne un mouvement à la balle vers la droite
-    balle.haut=balle.haut+vitesseY; //Donne un mouvement à la balle vers le bas
+    balle.gauche=balle.gauche + balle.vitesseX; //Donne un mouvement à la balle vers la droite
+    balle.haut=balle.haut + balle.vitesseY; //Donne un mouvement à la balle vers le bas
     
-    if(balle.gauche>terrain.largeur){
-        balle.gauche=terrain.largeur;
+    //Colisions avec le terrain
+
+    //bord droit
+    if(balle.gauche>terrain.largeur-balle.largeur){
+        balle.gauche=terrain.largeur-balle.largeur;
         balle.vitesseX=balle.vitesseX*-1;
     }
-    if(balle.gauche>terrain.largeur){
-        balle.gauche=terrain.largeur;
+    //bord gauche
+    if(balle.gauche<0){
+        balle.gauche=0;
         balle.vitesseX=balle.vitesseX*-1;
     }
-    if(balle.haut>terrain.hauteur){
-        balle.haut=terrain.hauteur;
+    //bord bas
+    if(balle.haut>terrain.hauteur-balle.hauteur){
+        balle.haut=terrain.hauteur-balle.hauteur;
         balle.vitesseY=balle.vitesseY*-1;
     }
-    if(balle.haut>terrain.hauteur){
-        balle.haut=terrain.hauteur;
+    //bord haut
+    if(balle.haut<0){
+        balle.haut=0;
         balle.vitesseY=balle.vitesseY*-1;
     }
     balle.majHTML();
